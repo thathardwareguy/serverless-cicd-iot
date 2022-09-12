f668c9a1cd02bf9bc17c5f0c45238427514cf5f7
thathardwareguy <ardutech11@gmail.com>
2022-09-12 07:59:30 +0100

    feat: edit led blank time

--- extra_script.py
+++ extra_script.py
@@ -8,7 +8,7 @@ Import("env")
 # access to global construction environment
 #print env
 build_tag = env['PIOENV']
-env.Replace(PROGNAME="firmware_1.0.1_%s"% build_tag)
+env.Replace(PROGNAME="firmware_1.1.0_%s"% build_tag)
 
 # Dump construction environments (for debug purpose)
 #print env.Dump()
