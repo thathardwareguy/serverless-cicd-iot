cad185fe1cbb7cdbb01c72ad1c7738997c57dd15
thathardwareguy <ardutech11@gmail.com>
2022-09-15 22:33:19 +0100

    Decrease led blink time

--- extra_script.py
+++ extra_script.py
@@ -8,7 +8,7 @@ Import("env")
 # access to global construction environment
 #print env
 build_tag = env['PIOENV']
-env.Replace(PROGNAME="firmware_2.2.0_%s"% build_tag)
+env.Replace(PROGNAME="firmware_2.3.0_%s"% build_tag)
 
 # Dump construction environments (for debug purpose)
 #print env.Dump()
