5c1c6043fc808bd63b59002f98179ff6eb17fa72
thathardwareguy <ardutech11@gmail.com>
2022-09-15 13:16:02 +0100

    feat: Decrease led blink rate

--- extra_script.py
+++ extra_script.py
@@ -8,7 +8,7 @@ Import("env")
 # access to global construction environment
 #print env
 build_tag = env['PIOENV']
-env.Replace(PROGNAME="firmware_1.5.0_%s"% build_tag)
+env.Replace(PROGNAME="firmware_1.6.0_%s"% build_tag)
 
 # Dump construction environments (for debug purpose)
 #print env.Dump()
