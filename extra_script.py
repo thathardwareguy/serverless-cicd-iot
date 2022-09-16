a2c9fa42c1b0d573548d4638a600b545799ead31
thathardwareguy <ardutech11@gmail.com>
2022-09-16 18:23:47 +0100

    feat: blink

--- extra_script.py
+++ extra_script.py
@@ -8,7 +8,7 @@ Import("env")
 # access to global construction environment
 #print env
 build_tag = env['PIOENV']
-env.Replace(PROGNAME="firmware_3.3.0_%s"% build_tag)
+env.Replace(PROGNAME="firmware_3.4.0_%s"% build_tag)
 
 # Dump construction environments (for debug purpose)
 #print env.Dump()
