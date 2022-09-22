ca4303a7d4944363ed53bc3442e574346119a21e
thathardwareguy <ardutech11@gmail.com>
2022-09-22 09:49:32 +0100

    fix: Adjust

--- backend/lambda/firmwareData.js
+++ backend/lambda/firmwareData.js
@@ -23,7 +23,7 @@ exports.handler = async (event) => {
     
     // Adding the items into the DynamoDB table
     try {
-        const {data} = await ddb.putItem(params).promise();
+        const {data} = await ddb.put(params).promise();
         console.log('Data:',data);
         return data;
     }
