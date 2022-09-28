4838280276c89cfe9ed193d0ed8f2538c8449239
thathardwareguy <ardutech11@gmail.com>
2022-09-28 15:06:01 +0100

    fix: Add print version

--- src/main.cpp
+++ src/main.cpp
@@ -204,6 +204,6 @@ void loop()
     ledState = ledState == LOW ? HIGH : LOW;
     digitalWrite(BUILTIN_LED, ledState);
   }
-Serial.println("Reading to test version 4.1.1....");
+Serial.println(RAWVERSION);
 delay(2000);
 }
